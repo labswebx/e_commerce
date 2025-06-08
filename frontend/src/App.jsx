6f91dd9e5c1871459e543aedfa1db99db1b19999
Khushi Kumari <khushikumarigupta463@gmail.com>
2025-06-08 19:33:57 +0530

    feat: add Footer component with styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./pages/header/Header";
 import { BrowserRouter as Router } from "react-router-dom";
+import Footer from "./pages/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Header />
+        <Footer />
       </Router>
     </div>
   );
---
+++ frontend/src/pages/Footer/Footer.jsx
@@ -0,0 +1,57 @@
+import { GitBranch, Joystick, VenetianMask, X } from "lucide-react";
+import Logo from "../../components/ui/Logo";
+
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-grid">
+        {/* Column 1 */}
+        <div className="footer-column">
+          <div className="flex items-center gap-2 mb-2 text-2xl font-bold">
+            <Logo text="Cyber" image="/logo-icon.jpg" size="lg" />
+          </div>
+          <p className="footer-description">
+            We are a residential interior design firm located in Portland. Our
+            boutique-studio offers more than
+          </p>
+          <div className="footer-icons">
+            <VenetianMask className="w-5 h-5 transition hover:text-white" />
+            <GitBranch className="w-5 h-5 transition hover:text-white" />
+            <Joystick className="w-5 h-5 transition hover:text-white" />
+            <X className="w-5 h-5 transition hover:text-white" />
+          </div>
+        </div>
+
+        {/* Column 2 */}
+        <div className="footer-column">
+          <h3 className="footer-title">Services</h3>
+          <ul className="footer-list">
+            <li>Bonus program</li>
+            <li>Gift cards</li>
+            <li>Credit and payment</li>
+            <li>Service contracts</li>
+            <li>Non-cash account</li>
+            <li>Payment</li>
+          </ul>
+        </div>
+
+        {/* Column 3 */}
+        <div className="footer-column">
+          <h3 className="footer-title">Assistance to the buyer</h3>
+          <ul className="footer-list">
+            <li>Find an order</li>
+            <li>Terms of delivery</li>
+            <li>Exchange and return of goods</li>
+            <li>Guarantee</li>
+            <li>Frequently asked questions</li>
+            <li>Terms of use of the site</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        © {new Date().getFullYear()} Cyber Studio. All rights reserved.
+      </div>
+    </footer>
+  );
+}
